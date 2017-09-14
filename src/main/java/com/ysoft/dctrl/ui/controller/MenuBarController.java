ff6b241cd3aa4ffbdcf3fa35606d958de73d51ad
Marek Kuhn <marek.kuhn@ysoft.com>
2017-09-14 12:44:20 +0200

    DEEC-114 # Menubar updated

--- src/main/java/com/ysoft/dctrl/ui/controller/MenuBarController.java
+++ src/main/java/com/ysoft/dctrl/ui/controller/MenuBarController.java
@@ -1,10 +1,22 @@
 package com.ysoft.dctrl.ui.controller;
 
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import com.sun.deploy.uitoolkit.impl.fx.HostServicesFactory;
+import com.sun.javafx.application.HostServicesDelegate;
+import javafx.application.Application;
+import javafx.application.HostServices;
+import javafx.scene.input.KeyCharacterCombination;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -35,10 +47,17 @@ public class MenuBarController extends LocalizableController implements Initiali
 
     private static final FileChooser.ExtensionFilter gcodeFilter = new FileChooser.ExtensionFilter("GCode file", GCODE_EXTENSION);
     private static final FileChooser.ExtensionFilter jobFilter = new FileChooser.ExtensionFilter("3D print job", JOB_EXTENSION);
+//    private final HostServicesDelegate hostServices = HostServicesFactory.getInstance();
 
 
     @FXML MenuItem settings;
     @FXML MenuItem exportAs;
+    @FXML MenuItem undo;
+    @FXML MenuItem redo;
+    @FXML MenuItem zoomIn;
+    @FXML MenuItem zoomOut;
+    @FXML MenuItem resetCamera;
+    @FXML MenuItem about;
 
     private RetentionFileChooser retentionFileChooser;
 
@@ -56,6 +75,27 @@ public class MenuBarController extends LocalizableController implements Initiali
         eventBus.subscribe(EventType.SCENE_SET_MODE.name(), (e) -> {
             exportAs.setDisable(e.getData() != SceneMode.GCODE);
         });
+
+        undo.setAccelerator(new KeyCodeCombination(KeyCode.Z, KeyCombination.CONTROL_DOWN));
+        undo.setOnAction( e -> System.out.println("UNDO"));
+        redo.setAccelerator(new KeyCodeCombination(KeyCode.Y, KeyCombination.CONTROL_DOWN));
+        redo.setOnAction( e -> System.out.println("REDO"));
+        zoomIn.setAccelerator(new KeyCodeCombination(KeyCode.ADD, KeyCombination.CONTROL_DOWN));
+        zoomIn.setOnAction( e -> System.out.println("zoomin"));
+        zoomOut.setAccelerator(new KeyCodeCombination(KeyCode.SUBTRACT, KeyCombination.CONTROL_DOWN));
+        zoomOut.setOnAction( e -> System.out.println("zoomOUT"));
+//        resetCamera.setAccelerator(new KeyCodeCombination(KeyCode.R, KeyCombination.CONTROL_DOWN));
+        resetCamera.setOnAction( e -> System.out.println("resetcam"));
+        about.setOnAction( e -> {
+            try {
+                Desktop.getDesktop().browse(new URI("https://www.ysoft.com/en/support-and-download"));
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            } catch (URISyntaxException e1) {
+                e1.printStackTrace();
+            }
+        });
+
 
         super.initialize(location, resources);
     }
