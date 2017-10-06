bb6e48693812fce478666bb613160c35f719b8e3
Tomáš Pilař <tomas.pilar@ysoft.com>
2017-10-06 09:45:14 +0200

    DEEC-41 Fix when config already exists in bin folder

--- src/main/java/com/ysoft/dctrl/slicer/cura/Cura.java
+++ src/main/java/com/ysoft/dctrl/slicer/cura/Cura.java
@@ -204,14 +204,15 @@ public class Cura implements Slicer {
                 });
             }
         });
-        
-        FileUtils.clearFolder(definitionDir);
-        try (BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream(configFile))){
+        try {
+            FileUtils.clearFolder(definitionDir);
             Stream<Path> configs = Files.list(Paths.get(definitionSourceDir));
             for(Path p : configs.toArray(Path[]::new)) {
                 Files.copy(p, Paths.get(definitionDir + File.separator + p.getFileName()), StandardCopyOption.REPLACE_EXISTING);
             }
-            deeControlContext.getObjectMapper().writeValue(os, printerProfile);
+            try (BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream(configFile))){
+                deeControlContext.getObjectMapper().writeValue(os, printerProfile);
+            }
         } catch (IOException e) {
             logger.error("Unable to create config files", e);
         }
